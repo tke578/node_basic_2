c4272cdfc9813214413181dc0f4143006352b925
Adnrew G <tke578@gmail.com>
2014-06-28 11:46:43 -0700

    Finished

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var number1 = 555;
 var number2 = 666;
-var math_functions = require('./module/mathlib.js')(number1,number2);
-
+var math_functions = require('./module/mathlib.js')(number1);
 console.log(math_functions.add(2,3));
 console.log(math_functions.multiply(2,3));
 console.log(math_functions.square(2));
+console.log(math_functions.random(1,35));
--- module/mathlib.js
+++ module/mathlib.js
@@ -1,13 +1,16 @@
-module.exports = function (num1,num2){
+module.exports = function (num1){
   return {
     add: function(num1, num2) { 
       return num1+num2;
        },
     multiply: function(num1, num2) {
-         return num1*num2;
+      return num1*num2;
        },
     square: function(num) {
-        return Math.pow(2,num);
+      return Math.pow(2,num);
+      },
+    random :function(num3,num4){
+      return Math.floor(Math.random() * (num3*num4) + num3);
     }
   }
 };
